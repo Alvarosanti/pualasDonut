7f8fccc7e0b5593b7ea1d6d82deafac3db23c450
Alvarosanti <alvarosantisteban56@gmail.com>
2023-09-22 21:00:38 -0500

    upload files

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Inicio',
+  },
+  {
+    path: 'feature',
+    label: 'Productos',
+  },
+  {
+    path: 'service',
+    label: 'Servicio',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonios',
+  },
+];
